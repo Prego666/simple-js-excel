0a7665f1b463f630903d2520b515505b79a88ffd
shvetsovgv <shvetsovgv@herzen.spb.ru>
2020-06-24 19:02:25 +0300

    finish DIP

--- src/core/routes/Router.js
+++ src/core/routes/Router.js
@@ -1,5 +1,6 @@
 import {$} from '../../core/dom';
 import {ActiveRoute} from './ActiveRoute';
+import {Loader} from '../../components/Loader';
 
 export class Router {
     constructor(selector, routes) {
@@ -8,6 +9,7 @@ export class Router {
         }
         this.$placeholder = $(selector);
         this.routes = routes;
+        this.loader = new Loader();
         this.page = null;
 
         this.changePageHandler = this.changePageHandler.bind(this);
@@ -20,15 +22,21 @@ export class Router {
         this.changePageHandler()
     }
 
-    changePageHandler() {
+    async changePageHandler() {
         if (this.page) {
             this.page.destroy();
         }
-        this.$placeholder.clear();
+        this.$placeholder.clear().append(this.loader);
+
         const Page = ActiveRoute.path.includes('excel') ?
             this.routes.excel : this.routes.dashboard;
+
         this.page = new Page(ActiveRoute.param);
-        this.$placeholder.append(this.page.getRoot());
+
+        const root = await this.page.getRoot();
+
+        this.$placeholder.clear().append(root);
+
         this.page.afterRender();
     }
 
